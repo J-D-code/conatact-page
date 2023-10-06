baf21ddfb8f81d6156beec1504a2301d6928d735
codeKing <113900804+J-D-code@users.noreply.github.com>
2023-10-06 11:03:14 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
 const button = document.querySelector('.btn');
 const ball = document.querySelector('.glow-ball');
 const form = document.getElementById('form');
-const message = document.getElementById('message');
+const Message = document.getElementById('message');
+const Name = document.getElementById('name');
+const Email = document.getElementById('email');
 
 // always remeber you can't use document.getElementByClassName and try to add a event listener to it, it justs doesn't work for some reasons
 
+
     button.addEventListener('click' , () =>{
-        setInterval(() =>{
-            ball.classList.add('animate')
+            setInterval(() =>{
+            ball.classList.add('animate');  
             
-        }, 1);
-        setTimeout(() =>{           
-            form.submit();                        
+            }, 1);
+            setTimeout( () =>{ 
+            if(Name.value && Email.value && Message.value){
+                form.submit();
+            }else{
+                alert('Please fill form before submitting'); 
+                
+            }
+          
+                
         }, 3000);
         ball.classList.remove('animate');
-        
-       }
-       );``
+    });``
